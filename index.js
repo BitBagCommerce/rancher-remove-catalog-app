53965d8cc77048d7f5c87c11ba2792b866bac201
Marcin Bonk <marvcin@gmail.com>
2022-02-03 23:19:06 +0100

    Bump

--- index.js
+++ index.js
@@ -3,20 +3,33 @@ const https = require('https');
 
 try {
     const options = {
-        hostname: core.getInput('rancherUrl'),
+        hostname: core.getInput('rancherUrl').replace(/http(s)?\:\/\//g, ''),
         port: 443,
         path: `/v1/catalog.cattle.io.clusterrepos/${core.getInput('appNamespace')}/${core.getInput('appName')}?action=uninstall`,
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${core.getInput('rancherToken')}`
-        },
-        json: data
+        }
     };
 
-    https
-        .request(options)
-        .on('error', error => core.setFailed(error.message));
+    const request = https.request(options, (response) => {
+        response.on('data', function (chunk) {
+            console.log('Got response')
+        });
+
+        response.on('end', function () {
+            if (response.statusCode > 299 || response.statusCode < 200) {
+                console.log("Api call failed with response code " + response.statusCode);
+            } else {
+                console.log('App removed');
+            }
+        });
+    });
+
+    request.write(data);
+    request.on('error', error => core.setFailed(error.message))
+    request.end();
 } catch (error) {
     core.setFailed(error.message)
 }
